fb8b97419ce552f2ba0a318dede4b4c876d70d58
faezah07 <faezah07@users.noreply.github.com>
2016-12-20 11:53:07 +0800

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,9 +1,28 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+## Download data
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
 
-#str(subSetData)
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+## Getting full dataset
+data_full <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 1
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
+
